3ff7b273ebda9a23b192d78f72746e166d3afbe3
demba003 <kd.debinski@gmail.com>
2017-05-09 14:33:21 +0200

    singleton

--- app/src/main/java/javalab/pk/saper/Controller/GameActivity.java
+++ app/src/main/java/javalab/pk/saper/Controller/GameActivity.java
@@ -40,8 +40,6 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
         czas = (TextView) findViewById(R.id.czas);
 
-        //plansza = new Plansza(this);          // domyslnie 6x6 i 6 bomb
-        plansza = new Plansza(this, 8, 4, 2);    //inne mozliwosci latwo dostepne
         planszaView = new PlanszaView(this, plansza, this, this);
         plansza.setPlanszaView(planszaView);
     }
